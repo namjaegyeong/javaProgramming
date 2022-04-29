bfc5ebaa294716165777d0125bb15a1193ddb5df
JaeGyeong <fb_olddog@naver.com>
2022-04-29 13:29:03 +0900

    4월 29일 수업 자료

--- src/daynamiccalltest/DynamicCallTest.java
+++ src/daynamiccalltest/DynamicCallTest.java
@@ -7,13 +7,13 @@ class Animal  {
 } 
  
 class Dog extends Animal { 
-  void sound() { // 메소드 오버라이딩, 부모 클래스의 sound()가 가려짐, 만약 가려지지 않았다면 부모 클래서의 sound()도 불러옴.
+  void sound() { // 메소드 오버라이딩
     System.out.println("왈왈"); 
   } 
 } 
  
 class Cat extends Animal { 
-  void sound() { // 메소드 오버라이딩, 부모 클래스의 sound()가 가려짐, 만약 가려지지 않았다면 부모 클래서의 sound()도 불러옴.
+  void sound() { // 메소드 오버라이딩
     System.out.println("야옹"); 
   } 
 } 
